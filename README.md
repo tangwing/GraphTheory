3211007a17fe72bf33bddb49b182b6cf1a5a0256
Léo SHANG <shang.france@gmail.com>
2012-12-06 11:03:37 +0100

    Init a new mini projet of several algorithms for calculating shortest path. And I start to use WTF License :)

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Graph Theory
 This is a collection of mini projects, which are all about Graph Theory!!!
 
 Search methods, minimum spanning tree, shortest path... and many other interesting things!
+
+Index:
+
+GraphCpp_Dijkstra, a backup of the course C++, with an implementation of Dijkstra under a untypical representation of graph.
+
+ShortestPath, implementations of some algorithms for searching the shortest path.
---
+++ ShortestPath/main.c
@@ -0,0 +1,19 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the WTF Public License, Version 2,
+ * as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ *
+ * This program is originally created by Leo SHANG,
+ * contact shang.france@gmail.com for more info.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "algo.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
